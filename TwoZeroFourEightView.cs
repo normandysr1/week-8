dbae334b8450f497ab3f9a501f6173519e297505
CupcakeMonster <flukersy@gmail.com>
2015-10-23 20:55:19 +0700

    Added code to show score on other form.

--- TwoZeroFourEightView.cs
+++ TwoZeroFourEightView.cs
@@ -12,9 +12,18 @@ namespace twozerofoureight
 {
     public partial class TwoZeroFourEightView : Form, View
     {
+        public int summation = 0;
         Model model;
         Controller controller;
-       
+
+        Score showScore = new Score();
+
+        public int Pass
+        {
+            get { return summation; }
+            set { summation = value; }
+        }
+
         public TwoZeroFourEightView()
         {
             InitializeComponent();
@@ -27,7 +36,37 @@ namespace twozerofoureight
 
         public void Notify(Model m)
         {
-            UpdateBoard(((TwoZeroFourEightModel) m).GetBoard());
+            UpdateBoard(((TwoZeroFourEightModel)m).GetBoard());
+            UpdateScore(((TwoZeroFourEightModel)m).GetBoard());
+            UpdateScore_Form(((TwoZeroFourEightModel)m).GetBoard());
+        }
+
+        private void UpdateScore(int[,] board)
+        {
+            int bScore = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    bScore += board[i, j];
+                }
+            }
+            lblScore.Text = Convert.ToString(bScore);
+        }
+
+        private void UpdateScore_Form(int[,] board)
+        {
+            int bScore = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    bScore += board[i, j];
+                }
+            }
+
+            showScore.F2Score.Text = Convert.ToString(bScore);
+            showScore.Visible = true;
         }
 
         private void UpdateTile(Label l, int i)
@@ -35,7 +74,9 @@ namespace twozerofoureight
             if (i != 0)
             {
                 l.Text = Convert.ToString(i);
-            } else {
+            }
+            else
+            {
                 l.Text = "";
             }
             switch (i)
@@ -60,25 +101,25 @@ namespace twozerofoureight
         }
         private void UpdateBoard(int[,] board)
         {
-            UpdateTile(lbl00,board[0, 0]);
-            UpdateTile(lbl01,board[0, 1]);
-            UpdateTile(lbl02,board[0, 2]);
-            UpdateTile(lbl03,board[0, 3]);
-            UpdateTile(lbl10,board[1, 0]);
-            UpdateTile(lbl11,board[1, 1]);
-            UpdateTile(lbl12,board[1, 2]);
-            UpdateTile(lbl13,board[1, 3]);
-            UpdateTile(lbl20,board[2, 0]);
-            UpdateTile(lbl21,board[2, 1]);
-            UpdateTile(lbl22,board[2, 2]);
-            UpdateTile(lbl23,board[2, 3]);
-            UpdateTile(lbl30,board[3, 0]);
-            UpdateTile(lbl31,board[3, 1]);
-            UpdateTile(lbl32,board[3, 2]);
-            UpdateTile(lbl33,board[3, 3]);
+            UpdateTile(lbl00, board[0, 0]);
+            UpdateTile(lbl01, board[0, 1]);
+            UpdateTile(lbl02, board[0, 2]);
+            UpdateTile(lbl03, board[0, 3]);
+            UpdateTile(lbl10, board[1, 0]);
+            UpdateTile(lbl11, board[1, 1]);
+            UpdateTile(lbl12, board[1, 2]);
+            UpdateTile(lbl13, board[1, 3]);
+            UpdateTile(lbl20, board[2, 0]);
+            UpdateTile(lbl21, board[2, 1]);
+            UpdateTile(lbl22, board[2, 2]);
+            UpdateTile(lbl23, board[2, 3]);
+            UpdateTile(lbl30, board[3, 0]);
+            UpdateTile(lbl31, board[3, 1]);
+            UpdateTile(lbl32, board[3, 2]);
+            UpdateTile(lbl33, board[3, 3]);
 
             //Update lblScore with summation of all number in the board.
-            int summation = 0;
+
             for (int row = 0; row < 4; row++)
             {
                 for (int column = 0; column < 4; column++)
@@ -88,7 +129,6 @@ namespace twozerofoureight
             }
 
             lblScore.Text = Convert.ToString(summation);
-
         }
 
         private void btnLeft_Click(object sender, EventArgs e)
@@ -111,5 +151,8 @@ namespace twozerofoureight
             controller.ActionPerformed(TwoZeroFourEightController.DOWN);
         }
 
+        private void TwoZeroFourEightView_Load(object sender, EventArgs e)
+        {
+        }
     }
 }
